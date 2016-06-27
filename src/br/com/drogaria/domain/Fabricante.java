374338b9c55d34e4bcbca749dd3704ddc10b789d
HelioRDF <jpfranca@bol.com.br>
2016-06-26 22:02:38 -0300

    Passo 72

---
+++ src/br/com/drogaria/domain/Fabricante.java
@@ -0,0 +1,39 @@
+package br.com.drogaria.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity // Indica que é uma entidade de Persistencia
+@Table(name = "tbl_fabricantes") // Informa que a entidade de Persistência está
+									// ligada a uma tabela no BD
+public class Fabricante {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "fab_codigo")
+	private Long codigo;
+
+	@Column(name = "fab_descricao", length = 50, nullable = false)
+	private String descricao; // fab_descricao
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
